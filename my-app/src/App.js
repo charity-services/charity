77e21d4b30872a0ed20f24e7a12bee14723e6156
majdiShomali <majdi.shomali.1997@gmail.com>
2023-06-15 17:00:34 +0300

    edit the files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,24 +3,26 @@ import { UserContext } from "./UserContext";
 import React, { useState, useEffect, useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+  //------------------  user  ----------------------- //
 import NavListMenu from "./components/Navbar";
-// import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
-// import Kitchen from "./pages/Kitchen";
-// import Admin from "./pages/Admin";
 import About from "./pages/aboutPage/About";
-// import Recipes from "./pages/Recipes";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/Login";
-// import ShowRecipe from "./pages/ShowRecipe";
-// import Kitchens from "./pages/Kitchens";
 
+//------------------  Beneficiary  ----------------------- //
+import HomeBeneficiary from "./pages/beneficiary/HomeBeneficiary"
+
+
+
+
+//------------------ Dashboard  ----------------------- //
 
 import Sidebar from "./pages/dashboard/Sidebar";
 import NavListMenuD from "./pages/dashboard/NavDashboard";
 import MainDashboard from "./pages/dashboard/MainDashboard";
-// import UserProfile from "./pages/UserProfile";
+
 import UserInfo from "./components/dashboard/UserInfo"
 import ApproveTable from "./components/dashboard/ApproveTable";
 import AdminInfo from "./components/dashboard/AdminInfo";
@@ -82,6 +84,7 @@ export default function App() {
     }
   }, []);
 
+  //------------------  user Router ----------------------- //
   const AppRouter1 = () => {
     return (
       <Router>
@@ -92,17 +95,14 @@ export default function App() {
           <Route path="About" element={<About />} /> 
           <Route path="SignUp" element={<SignUp />} />
           <Route path="LogIn" element={<LogIn />} />
-          {/* <Route path="ShowRecipe" element={<ShowRecipe />} />
-          <Route path="Recipes" element={<Recipes />} />
-          <Route path="Kitchen" element={<Kitchen />} />
-          <Route path="UserProfile" element={<UserProfile />} />
-          <Route path="/Kitchen/:type_Kitchen" element={<Kitchens />} /> */}
+
         </Routes>
         <Footer />
       </Router>
     );
   };
 
+   //------------------  Dashboard Router ----------------------- //
   const AppRouter2 = () => {
     return (
       <Router>
@@ -121,18 +121,19 @@ export default function App() {
     );
   };
 
+  //------------------  Beneficiary Router ----------------------- //
   const AppRouter3 = () => {
     return (
       <Router>
-        {/* <NavListMenu /> */}
+        <NavListMenu />
         <Routes>
-          {/* <Route index element={<Admin />} />
+          <Route index element={<HomeBeneficiary />} />
           <Route path="ContactUs" element={<Contact />} />
           <Route path="About" element={<About />} />
-          <Route path="SignUp" element={<SignUp />} />
+          {/* <Route path="SignUp" element={<SignUp />} />
           <Route path="LogIn" element={<LogIn />} /> */}
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     );
   };
